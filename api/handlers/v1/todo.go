00512fd4318e2efd0dc490f5b6a60485ab1d93fb
Madiyor <m.abdukhashimov@gmail.com>
2021-04-25 14:25:27 +0500

    added todo-items

--- api/handlers/v1/todo.go
+++ api/handlers/v1/todo.go
@@ -8,6 +8,15 @@ func (h *handlerV1) CreateNewTodo(c *gin.Context) {
 
 }
 
+// @Router /v1/todo [get]
+// @Summary Get Daily View Count
+// @Description API for getting daily view count
+// @Tags analytics
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} models.GetDailyViewCountResponse
+// @Failure 404 {object} models.ResponseError
+// @Failure 500 {object} models.ResponseError
 func (h *handlerV1) GetAllTodo(c *gin.Context) {
 
 }
--- api/models/todo.go
+++ api/models/todo.go
@@ -1,6 +1,10 @@
 package models
 
-type TodoModel struct {
+type SingleTodoModel struct {
 	TaskName   string `json:"task_name"`
 	TaskStatus string `json:"task_status"`
+}
+
+type AllTodoModel struct {
+	Todos []SingleTodoModel `json:"todo_items"`
 }
