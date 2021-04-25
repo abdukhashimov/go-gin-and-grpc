d697af68f783dabc5be1c4f87c496cc874a81f52
Madiyor <m.abdukhashimov@gmail.com>
2021-04-25 14:21:08 +0500

    Added grpc client

--- pkg/grpc_client/client.go
+++ pkg/grpc_client/client.go
@@ -1,24 +1,40 @@
 package grpc_client
 
 import (
+	"fmt"
+
 	"github.com/abdukhashimov/go_gin_example/config"
+	"github.com/abdukhashimov/go_gin_example/genproto/todo_service"
+	"google.golang.org/grpc"
 )
 
 //GrpcClientI ...
 type GrpcClientI interface {
+	ToDoService() todo_service.TodoServiceClient
 }
 
 //GrpcClient ...
 type GrpcClient struct {
-	cfg         config.Config
-	connections map[string]interface{}
+	todoService todo_service.TodoServiceClient
 }
 
 //New ...
 func New(cfg config.Config) (*GrpcClient, error) {
 
+	todoService, err := grpc.Dial(
+		fmt.Sprintf("%s:%d", cfg.TodoServiceHost, cfg.TodoServicePort),
+		grpc.WithInsecure(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
 	return &GrpcClient{
-		cfg:         cfg,
-		connections: map[string]interface{}{},
+		todoService: todo_service.NewTodoServiceClient(todoService),
 	}, nil
+}
+
+func (g *GrpcClient) TodoService() todo_service.TodoServiceClient {
+	return g.todoService
 }
