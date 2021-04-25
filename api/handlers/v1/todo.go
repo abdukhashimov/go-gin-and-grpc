702b511e96e99526405aa16de29ea2fcb6845cbb
Madiyor <m.abdukhashimov@gmail.com>
2021-04-25 14:38:12 +0500

    swagger is working now

--- api/handlers/v1/todo.go
+++ api/handlers/v1/todo.go
@@ -9,9 +9,9 @@ func (h *handlerV1) CreateNewTodo(c *gin.Context) {
 }
 
 // @Router /v1/todo [get]
-// @Summary Get Daily View Count
-// @Description API for getting daily view count
-// @Tags analytics
+// @Summary Get List of Todo
+// @Description API to retreive list of todo
+// @Tags TODO
 // @Accept  json
 // @Produce  json
 // @Success 200 {object} models.AllTodoModel
@@ -21,14 +21,41 @@ func (h *handlerV1) GetAllTodo(c *gin.Context) {
 
 }
 
+// @Router /v1/todo/{id} [get]
+// @Summary Get a Todo
+// @Description API to retreive a single todo
+// @Tags TODO
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} models.SingleTodoModel
+// @Failure 404 {object} models.ResponseError
+// @Failure 500 {object} models.ResponseError
 func (h *handlerV1) GetTodo(c *gin.Context) {
 
 }
 
+// @Router /v1/todo/{id} [put]
+// @Summary Get a Todo
+// @Description API to retreive a single todo
+// @Tags TODO
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} models.Response
+// @Failure 404 {object} models.ResponseError
+// @Failure 500 {object} models.ResponseError
 func (h *handlerV1) UpdateTodo(c *gin.Context) {
 
 }
 
+// @Router /v1/todo/{id} [get]
+// @Summary Get a Todo
+// @Description API to retreive a single todo
+// @Tags TODO
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} models.Response
+// @Failure 404 {object} models.ResponseError
+// @Failure 500 {object} models.ResponseError
 func (h *handlerV1) DeleteTodo(c *gin.Context) {
 
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/abdukhashimov/go_gin_example
 go 1.16
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.1
@@ -11,6 +12,7 @@ require (
 	github.com/spf13/cast v1.3.1
 	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
 	github.com/swaggo/gin-swagger v1.3.0
+	github.com/swaggo/swag v1.5.1
 	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	google.golang.org/grpc v1.37.0
